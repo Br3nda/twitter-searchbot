be620fea185cc72c2f8427da6c8013f8c6ae67ba
brenda <brenda@tinyshiny.(none)>
2008-12-06 13:52:12 +1300

    added god2usage, and getopts

--- bot.pl
+++ bot.pl
@@ -3,70 +3,98 @@
 #create table repeated( id integer UNIQUE NOT NULL);
 
 use strict;
-use Net::Twitter;
-use Net::Twitter::Diff;
+#use Net::Twitter;
+#use Net::Twitter::Diff;
 use Net::Twitter::Search;
-use Data::Dumper;
-use DBI;
+#use Data::Dumper;
+#use DBI;
+use Getopt::Long qw(GetOptions);
+use Pod::Usage;
 
 
-my $dsn    = "dbi:Pg:dbname=bots";
-my $dbh    = DBI->connect($dsn, 'brenda', '')
-  or unlock_die ("Cannot connect to the database '$dsn': $DBI::errstr");
+#my $dsn    = "dbi:Pg:dbname=bots";
+#my $dbh    = DBI->connect($dsn, 'brenda', '')
+#  or unlock_die ("Cannot connect to the database '$dsn': $DBI::errstr");
 
-my $username = $ARGV[0];
-my $password = $ARGV[1]; 
-my $trigger =  $ARGV[2]; 
+my ($username, $password, $trigger);
+my $help = 0; 
+my $max = 1;
 
-die ("Username and Password and trigger required\n") unless ($username && $password && $trigger);
+my $result = GetOptions(
+	'help' => \$help, 
+	'username=s' => \$username, 
+	'password=s' => \$password, 
+	'trigger=s' => \$trigger, 
+	'max=s' => \$max,
 
-my $twit = Net::Twitter::Diff->new(  username => $username, password => $password);
+);
+
+pod2usage(-exitval => 0, -verbose => 2) if($help);
+pod2usage(-exitval => 1, -verbose => 1) unless ($username && $password && $trigger); 
+
 my $search = Net::Twitter::Search->new(  username => $username, password => $password);
 
-my %repeated = {};
+print "Searching for $trigger, max results $max\n";
+my $results = $search->search($trigger);
 
-my @result = getRecordSet('SELECT * FROM repeated ORDER BY id DESC LIMIT 1000');
-foreach my $t (@result) {
-  $repeated{$t->{id}} = 1;
-}
+my $repeats_thus_far = 0;
 
-
-	my $results = $search->search($trigger);
- 	foreach my $tweet (@{ $results }) {
- 		my $speaker =  $tweet->{from_user};
-        	my $text = $tweet->{text};
- 		my $time = $tweet->{created_at};
-       	 	print "$time <$speaker> $text\n";
- 		if($repeated{$tweet->{id}}) {
- 			print "\t Already repeated\n";
- 		}
- 		elsif ($speaker =~ m/$username/i) {
- 			print "\tThat's me!\n";
- 		}
- 		else {
- 			my $repeat = $text;
- 			chomp($repeat);
- 			$repeat = "\@$speaker says $repeat";
- 			print "*** \tRepeating: $repeat\n";
- 			$twit->update($repeat);
- 			my $sQuery = $dbh->prepare('INSERT INTO repeated (id) VALUES (?)');
- 			$sQuery->execute($tweet->{id});
- 			$repeated{$tweet->{id}} = 1;
-  	  	}
- 	}
-
-
-sub getRecordSet {
-    my $query = shift;
-    my $sQuery = $dbh->prepare($query);
-    $sQuery->execute or die ("Error in query:\n $query\n");
-
-    my $hRecordSet;
-    my @aReturnRecordSet;
-    while ( my $sRecordSet = $sQuery->fetchrow_hashref()) {
-        push(@aReturnRecordSet,$sRecordSet);
+foreach my $tweet (@{ $results }) {
+	my $speaker =  $tweet->{from_user};
+	my $text = $tweet->{text};
+	my $time = $tweet->{created_at};
+	print "$time <$speaker> $text\n";
+	if ($text =~ /^\@/) {
+      print "Skipping reply\n";
     }
+	elsif ($speaker =~ m/$username/i) {
+		print "\tThat's me!\n";
+	}
+	else {
+		my $repeat = $text;
+		chomp($repeat);
+		$repeat = "\@$speaker <3! $repeat";
+		print "*** \tRepeating: $repeat\n";
+		$search->update($repeat);
+		$repeats_thus_far++;
+	}
 
-    return (@aReturnRecordSet);
+	if ($repeats_thus_far >= $max) {
+		exit;
+	}
 }
+
+__END__
+
+=head1 NAME
+
+bot.pl - Searches for a word/phrase on twitter, and then repeats the whole tweet to a twitter account
+
+=head1 SYNOPSIS
+
+  bot.pl --username=<username> --password=<password> --trigger="<phrase>"
+
+=head1 OPTIONS
+
+  -? --help Verbose help 
+
+=head1 DESCRIPTION
+
+To aggregate all posts on a subject into a single twitter account for following.
+
+e.g. if you want to follow all tweets on "erlang", use erlang as you trigger, and create an account for this script to tweet into. By following this account you can keep up with erlang tweets
+
+or, if you are running a conference, use the name of the conference as a trigger, and suggest conference attendees follow your official conference twitter account.
+
+=head1 EXAMPLES
+
+  ./bot.pl --username=DreadPirateRoberta --password=asyouwish --trigger="super happy dev house"
+
+=head1 CREDIT
+
+This script by Brenda Wallace
+http://coffee.geek.nz
+
+=cut
+
 
